f2288dd3b2bbd1c6db2bad84db016379a193d87e
rahul <iamrahulyadav97@gmail.com>
2020-12-18 10:50:34 +0530

    added country flags

--- server.js
+++ server.js
@@ -1,2 +1,26 @@
 const express = require("express");
-const handlebars = require("express-handlebars");
+
+const app = express();
+
+var handlebars =require("express-handlebars").create({defaultLayout:"main"})
+ const api = require("novelcovid");
+
+ app.engine("handlebars",handlebars.engine);
+ app.set("view engine","handlebars");
+//  api.all().then(response=>{
+//      console.log(response);
+//  })
+app.get("/",(req,res)=>
+{
+   
+    api.countries().then(response=>{
+        console.log(response);
+        res.render("home",{ info:response});
+          })
+
+})
+
+ app.listen(3000,()=>{
+     console.log("Listening on 3000");
+ })
+ 
